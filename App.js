9d894e7f990e00739b1e3fee811864ec82333297
Sean James <seanjames0114@gmail.com>
2021-09-28 22:10:34 -0400

    Changed there to be an imaged displayed

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'; //Safe area view will allow content to be in the safe area for each device 
 
 export default function App() {
-let x = 1;
   console.log("App has been executed") // This is how you can log to the debugger
   return (
-    <View style={styles.container}>
-      <Text>This will be the page the app will open on. I just needed to get it up and running.</Text>
+    <SafeAreaView style={styles.container}> 
+      <Text>This will be the page the app will open on. I just needed to get it up and running.</Text> 
       <Text>I hope you like it :)</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image source={{
+        width : 200,
+        height : 300,
+        uri: "https://picsum.photos/200/300"}} />
+      <Text>There should be an image above me</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //flexible to grow and take it up horizontaller
+    backgroundColor: '#fff', //can use name or rgb colors 
     alignItems: 'center',
     justifyContent: 'center',
   },
