692f680c40ec5ba9790cad5d05a50fcbfaee049d
Sean James <seanjames0114@gmail.com>
2021-09-28 20:28:20 -0400

    Update Page

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This will be the page the app will open on. I just needed to get it up and running.</Text>
+      <Text>I hope you like it :)</Text>
       <StatusBar style="auto" />
     </View>
   );
